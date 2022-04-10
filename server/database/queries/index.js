b4ae53aa4a589a6abade82aa8ea0b45465168107
israaHamdi <israaaburayya2012@gmail.com>
2022-04-10 13:11:23 +0200

    handle queries

---
+++ server/database/queries/index.js
@@ -0,0 +1,11 @@
+const addBookQuery = require('./addBookQuery');
+const deleteBookQuery = require('./deleteBookQuery');
+const getBooksQuery = require('./getBooksQuery');
+const getSingleBookQuery = require('./getSingleBookQuery');
+
+module.exports = {
+  addBookQuery,
+  deleteBookQuery,
+  getBooksQuery,
+  getSingleBookQuery,
+};
