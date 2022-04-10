68aaab870f854b19e3f1fb360be7da8b6029248d
karam-zomlut <zomlutk@gmail.com>
2022-04-11 02:26:33 +0300

    Add Filter By Category

--- client/src/Components/Books/Filter/Filter.jsx
+++ client/src/Components/Books/Filter/Filter.jsx
@@ -5,6 +5,21 @@ const Filter = ({ categories, ctgType, minPrice, maxPrice, handleChange }) => {
   return (
     <form className='filter-form'>
       <h2 className='form-title'>Filter</h2>
+      <div className='categories filter-ctg'>
+        <h3 className='filter-type'>Categories</h3>
+        {categories.map((ctg, index) => (
+          <label className='single-category' key={index}>
+            <input
+              type='radio'
+              name='ctgType'
+              value={ctg}
+              checked={ctgType === ctg}
+              onChange={(e) => handleChange(e)}
+            />
+            {ctg}
+          </label>
+        ))}
+      </div>
       
     </form>
   );
