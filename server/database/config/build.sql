0afd1b953281092a1c57d1626903953a90f71da4
braaAwni <braaberoo95@gmail.com>
2022-04-10 17:12:08 +0300

    add author

--- server/database/config/build.sql
+++ server/database/config/build.sql
@@ -6,16 +6,18 @@ create table Books (
     description text not null,
     price int not null,
     category varchar(100) not null,
+    author varchar(100) not null,
     imageUrl text not null
 );
 INSERT INTO
-    Books (name, description, price, category, imageUrl)
+    Books (name, description, price, category, author, imageUrl)
     values
     (
         'The Diary of a Young Girl',
         'Neque porro quisquam est qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit',
         14.5,
         'History',
+        'Anne Frank',
         'https://prodimage.images-bn.com/lf?set=key%5Bresolve.pixelRatio%5D,value%5B1%5D&set=key%5Bresolve.width%5D,value%5B550%5D&set=key%5Bresolve.height%5D,value%5B10000%5D&set=key%5Bresolve.imageFit%5D,value%5Bcontainerwidth%5D&set=key%5Bresolve.allowImageUpscaling%5D,value%5B0%5D&product=path%5B/pimages/9789352782772_p0_v1%5D&call=url%5Bfile:common/decodeProduct.chain%5D'
     ),
     (
@@ -23,6 +25,7 @@ INSERT INTO
         'Neque porro quisquam est qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit',
         50,
         'Classics',
+        'Elie Wiesel',
         'https://i.gr-assets.com/images/S/compressed.photo.goodreads.com/books/1575073611l/1617.jpg'
     ),
     (
@@ -30,6 +33,7 @@ INSERT INTO
         'Neque porro quisquam est qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit',
         19.9,
         'Arts',
+        'Harper Lee',
         'https://prodimage.images-bn.com/lf?set=key%5Bresolve.pixelRatio%5D,value%5B1%5D&set=key%5Bresolve.width%5D,value%5B550%5D&set=key%5Bresolve.height%5D,value%5B10000%5D&set=key%5Bresolve.imageFit%5D,value%5Bcontainerwidth%5D&set=key%5Bresolve.allowImageUpscaling%5D,value%5B0%5D&product=path%5B/pimages/9780060935467_p0_v12%5D&call=url%5Bfile:common/decodeProduct.chain%5D'
     ),
     (
@@ -37,6 +41,7 @@ INSERT INTO
         'Neque porro quisquam est qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit',
         200,
         'Classics',
+        'F.Scott Fitzgerald',
         'https://m.media-amazon.com/images/M/MV5BMTkxNTk1ODcxNl5BMl5BanBnXkFtZTcwMDI1OTMzOQ@@._V1_FMjpg_UX999_.jpg'
     );
 COMMIT;
