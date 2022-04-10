dad6cf5fa98b4f0fc299b693f405ebdba0f49ac3
israaHamdi <israaaburayya2012@gmail.com>
2022-04-10 12:25:14 +0200

    handle errors

--- server/app.js
+++ server/app.js
@@ -2,6 +2,7 @@ require('dotenv').config();
 const { join } = require('path');
 const express = require('express');
 const apiRouter = require('./router');
+const { clientError, serverError } = require('./controller/error');
 
 const app = express();
 
