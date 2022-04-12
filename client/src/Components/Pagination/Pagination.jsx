79b5c5d762fb62205bb236d4a802a4a3c83eea22
karam-zomlut <zomlutk@gmail.com>
2022-04-12 21:19:39 +0300

    Edit Pagination Style

--- client/src/Components/Pagination/Pagination.jsx
+++ client/src/Components/Pagination/Pagination.jsx
@@ -11,12 +11,27 @@ const Pagination = ({ postsPerPage, totalPosts, paginate }) => {
   return (
     <nav className='nav-pagination'>
       <ul className='pagination'>
-        {pageNumbers.length>1?  pageNumbers.map(number => (
-          <li key={number} className='page-item'>
-              <button onClick={() => paginate(number)} className='page-link'>{number}</button>
-              
-          </li>
-        )):null}
+        {pageNumbers.length>1?
+        <>
+        <li className='page-item prev'>
+          <button className='page-link'>
+            <i class="ri-arrow-left-s-line"></i>
+          </button>
+        </li>
+        {
+          pageNumbers.map(number => (
+            <li key={number} className='page-item'>
+                <button onClick={() => paginate(number)} className='page-link'>{number}</button>
+            </li>
+          ))
+        }
+        <li className='page-item next'>
+          <button className='page-link'>
+            <i class="ri-arrow-right-s-line"></i>
+          </button>
+        </li>
+        </>
+        : null}
       </ul>
     </nav>
   );
