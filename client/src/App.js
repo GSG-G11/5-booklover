e7682875596e736ac985badb9a6be52d97cf76d4
karam-zomlut <zomlutk@gmail.com>
2022-04-13 00:15:53 +0300

    Clear Inputs When User Login

--- client/src/App.js
+++ client/src/App.js
@@ -42,6 +42,7 @@ class App extends Component {
     this.setState({ isLogin: true });
     const { nameSeller } = this.state;
     localStorage.setItem('nameSeller', nameSeller);
+    this.setState({nameSeller: '', passwordSeller: ''})
     this.handleLogin(e);
   };
   handleLogin = (e) => {
